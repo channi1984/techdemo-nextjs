63479cb8cf56c0162e7c5effd2ca6696754d1908
channi84 <channi.kr@gmail.com>
2025-07-29 14:19:43 +0900

    수정

---
+++ app/features/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Features() {
+	return (
+		<main>
+			여기는 서브 입니다
+		</main>
+	);
+}
--- app/page.js
+++ app/page.js
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Main() {
 	return (
-		<main>
-
-		</main>
+		<div className="wrap-main">
+			<div className="cart">
+				카트
+			</div>
+			<div className="desc">
+				제품 소개
+			</div>
+		</div>
 	);
 }
