d3f23bab14f2a79bf734f262fbef6e339bbecd6e
channi84 <channi.kr@gmail.com>
2025-07-30 08:53:41 +0900

    수정

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
 							<div className="menu">
 								<ul>
 									<li>
-										<Link href="/features">Features</Link>
+										<Link href="/movie">Movie</Link>
 									</li>
 									<li>
 										<Link href="/specifications">Specifications</Link>
---
+++ app/movie/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function MovieFeatures() {
+	return (
+		<div className="desc">
+			{/* 무비 */}
+			오늘은 무비 페이지를 만들어 보자
+		</div>
+	);
+}
