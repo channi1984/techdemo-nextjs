c6bbd53f8fea6cf6f0b42e5eee107f2521e7d207
channi84 <channi.kr@gmail.com>
2025-07-29 16:15:17 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -85,6 +85,97 @@ export default function Main() {
 									<strong>$699</strong>
 									<button type="button">Buy Now</button>
 								</div>
+								<div className="icons">
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div className="thumb">
+									<Image src="/images/img-thumb.png" alt="썸네일 이미지" width={505} height={505} />
+								</div>
+								<div className="info">
+									<span>Model A</span>
+									<strong>$699</strong>
+									<button type="button">Buy Now</button>
+								</div>
+								<div className="icons">
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div className="thumb">
+									<Image src="/images/img-thumb.png" alt="썸네일 이미지" width={505} height={505} />
+								</div>
+								<div className="info">
+									<span>Model A</span>
+									<strong>$699</strong>
+									<button type="button">Buy Now</button>
+								</div>
+								<div className="icons">
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+									<div className="box">
+										<div className="icon">
+											<Image src="/images/ic-phone3.png" alt="아이콘" width={121} height={100} />
+										</div>
+										<strong>Camera</strong>
+										<p>12MP</p>
+									</div>
+								</div>
 							</li>
 						</ul>
 					</div>
