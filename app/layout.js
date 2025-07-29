f8c3b03f8c489fda43e0c6b01362d6b1f4dd6938
channi84 <channi.kr@gmail.com>
2025-07-29 13:57:05 +0900

    수정

--- app/layout.js
+++ app/layout.js
@@ -13,38 +13,43 @@ export default function RootLayout({ children }) {
 			<body>
 				{/* 헤더 */}
 				<header className="wrap-header">
-					<div className="logo">
-						<Image src="/images/ic-logo.png" alt="로고" width={356} height={44} />
-					</div>
-					<div className="menu">
-						<ul>
-							<li>
-								<a href="#link">Features</a>
-							</li>
-							<li>
-								<a href="#link">Specifications</a>
-							</li>
-							<li>
-								<a href="#link">Reviews</a>
-							</li>
-							<li>
-								<a href="#link">Support</a>
-							</li>
-						</ul>
-					</div>
-					<div className="util">
-						<div className="price">
-
+					<div className="area">
+						<div className="logo">
+							<Image src="/images/ic-logo.png" alt="로고" width={356} height={44} />
 						</div>
-						<div className="buy">
-							
+						<div className="menu">
+							<ul>
+								<li>
+									<a href="#link">Features</a>
+								</li>
+								<li>
+									<a href="#link">Specifications</a>
+								</li>
+								<li>
+									<a href="#link">Reviews</a>
+								</li>
+								<li>
+									<a href="#link">Support</a>
+								</li>
+							</ul>
+						</div>
+						<div className="util">
+							<div className="price">
+								<span>Starting from</span>
+								<span>$999</span>
+							</div>
+							<div className="buy">
+								<button type="button">Buy now</button>
+							</div>
 						</div>
 					</div>
 				</header>
 
 				{/* 바디 */}
 				<main className="wrap-con">
-					{children}
+					<div className="area">
+						{children}
+					</div>
 				</main>
 			</body>
 		</html>
