04fcda48b1f1a41410f64acd673a552be8daf0a8
channi84 <channi.kr@gmail.com>
2025-07-29 14:25:51 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -4,7 +4,41 @@ export default function Main() {
 	return (
 		<div className="wrap-main">
 			<div className="cart">
-				카트
+				<div className="title">
+					<strong>TechMaster Pro</strong>
+					<p>$999</p>
+				</div>
+
+				<div className="color">
+					<ul>
+						<li className="color1">
+							<button type="button"></button>
+						</li>
+						<li className="color2">
+							<button type="button"></button>
+						</li>
+						<li className="color3">
+							<button type="button"></button>
+						</li>
+						<li className="color4">
+							<button type="button"></button>
+						</li>
+					</ul>
+				</div>
+
+				<div className="size">
+					<ul>
+						<li>
+							<button type="button">medium</button>
+						</li>
+						<li>
+							<button type="button">medium</button>
+						</li>
+						<li>
+							<button type="button">medium</button>
+						</li>
+					</ul>
+				</div>
 			</div>
 			<div className="desc">
 				제품 소개
