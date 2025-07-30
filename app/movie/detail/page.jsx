da40b7357dc673e96d87f1429b12596cdb735ad7
channi84 <channi.kr@gmail.com>
2025-07-30 14:15:31 +0900

    1

--- app/movie/detail/page.jsx
+++ app/movie/detail/page.jsx
@@ -7,7 +7,7 @@ export default function MovieDetail() {
 			<div className="wrap-movie-detail">
 				{/* 영화 표지 */}
 				<div className="thumb">
-					<Image src="/images/img-movie.png" alt="커버 이미지" width={860} height={964} />
+					<Image src="/images/img-movie-detail.png" alt="커버 이미지" width={860} height={964} />
 					<div className="play">
 						<button type="button">플레이 버튼</button>
 					</div>
@@ -22,6 +22,65 @@ export default function MovieDetail() {
 					<button type="button" className="like">
 						<Image src="/images/ic-movie-like.png" alt="하트 이미지" width={50} height={45} />
 					</button>
+				</div>
+
+				<div className="info">
+					<div className="title">
+						Under Paris
+					</div>
+					<div className="desc">
+						To save Paris from a bloodbath, a grieving scientist is forced to face her
+						tragic past when a giant shark appears in the Seine. <strong>Show More</strong>
+					</div>
+				</div>
+
+				<div className="actor">
+					<div className="title">
+						Actors
+					</div>
+					<div className="list">
+						<ul>
+							<li>
+								<div className="thumb">
+									<Image src="/images/img-movie-actor.png" alt="배우 이미지" width={216} height={216} />
+								</div>
+								<div className="name">
+									Nassim Lyes
+								</div>
+							</li>
+
+							<li>
+								<div className="thumb">
+									<Image src="/images/img-movie-actor.png" alt="배우 이미지" width={216} height={216} />
+								</div>
+								<div className="name">
+									Nassim Lyes
+								</div>
+							</li>
+
+							<li>
+								<div className="thumb">
+									<Image src="/images/img-movie-actor.png" alt="배우 이미지" width={216} height={216} />
+								</div>
+								<div className="name">
+									Nassim Lyes
+								</div>
+							</li>
+
+							<li>
+								<div className="thumb">
+									<Image src="/images/img-movie-actor.png" alt="배우 이미지" width={216} height={216} />
+								</div>
+								<div className="name">
+									Nassim Lyes
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div className="button">
+					<button type="button">Open</button>
 				</div>
 			</div>
 		</div>
