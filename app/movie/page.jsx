60fa1f522a52ce1aa035456e6971178be2dd6425
channi84 <channi.kr@gmail.com>
2025-07-30 09:34:11 +0900

    수정

--- app/movie/page.jsx
+++ app/movie/page.jsx
@@ -5,7 +5,29 @@ export default function MovieFeatures() {
 		<div className="desc">
 			{/* 무비 */}
 			<div className="wrap-movie">
+				{/* 헤더 */}
+				<div className="header">
+					<div className="people">
+						<div className="thumb">
 
+						</div>
+						<div className="info">
+							<span>Welcome back</span>
+							<strong>Aymen Missaoui</strong>
+						</div>
+					</div>
+
+					<div className="menu">
+						<button type="button"></button>
+					</div>
+				</div>
+
+				{/* 서치 */}
+				<div className="search">
+					<button type="button" className="search">검색버튼</button>
+					<input type="text" placeholder="Search" />
+					<button type="button" className="filter">필터버튼</button>
+				</div>
 			</div>
 		</div>
 	);
