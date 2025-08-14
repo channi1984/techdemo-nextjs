4b2c3af81e83df72facdd6d326d9a51e84773a9b
channi84 <channi.kr@gmail.com>
2025-08-14 11:26:35 +0900

    A

--- app/music/page.jsx
+++ app/music/page.jsx
@@ -1,11 +1,11 @@
 
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Music() {
 	return (
 		<div className="desc">
 			{/* 뮤직 */}
-			나는 뮤직앱이야
 		</div>
 	);
 }
