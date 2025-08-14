323c4aed6685f20c4faa44083771eb6d38750ada
channi84 <channi.kr@gmail.com>
2025-08-14 14:07:22 +0900

    뮤직앱 퍼블리싱

--- app/music/page.jsx
+++ app/music/page.jsx
@@ -9,6 +9,16 @@ export default function Music() {
 			<div className="wrap-music">
 				{/* 커버 */}
 				<div className="cover">
+					<div className="header">
+						<div className="noti">
+							<button type="button" className="btn-noti">
+								<Image src="/images/ic-music-noti.png" alt="알림" width={80} height={80} />
+							</button>
+						</div>
+						<div className="player">
+							<Image src="/images/img-music-player.png" alt="플레이어" width={80} height={80} />
+						</div>
+					</div>
 					<div className="title">
 						<strong>JOHN WICK</strong>
 					</div>
@@ -16,23 +26,208 @@ export default function Music() {
 						<div className="util">
 							<ul>
 								<li className="like">
-									<button type="button" className="btn-like">좋아요</button>
+									<button type="button" className="btn-like">
+										<Image src="/images/ic-music-like.png" alt="좋아요" width={50} height={46} />
+									</button>
 								</li>
 								<li className="share">
-									<button type="button" className="btn-share">공유하기</button>
+									<button type="button" className="btn-share">
+										<Image src="/images/ic-music-share.png" alt="공유하기" width={40} height={46} />
+									</button>
 								</li>
 							</ul>
 						</div>
-						<div className="play">
+						<div className="control">
 							<ul>
 								<li className="play">
-									<button type="button" className="btn-play">재생하기</button>
+									<button type="button" className="btn-play">
+										<Image src="/images/ic-music-all-play.png" alt="플레이" width={160} height={160} />
+									</button>
 								</li>
 								<li className="shuffle">
-									<button type="button" className="btn-shuffle">셔플재생</button>
+									<button type="button" className="btn-shuffle">
+										<Image src="/images/ic-music-shuffle.png" alt="셔플 재생" width={54} height={54} />
+									</button>
 								</li>
 								<li className="more">
-									<button type="button" className="btn-more">추가 메뉴</button>
+									<button type="button" className="btn-more">
+										<Image src="/images/ic-music-more.png" alt="더보기" width={14} height={54} />
+									</button>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div className="play-list">
+					<div className="util">
+						<div className="total">
+							<em>15</em> Songs
+						</div>
+						<div className="search">
+							<button type="button" className="btn-search">
+								<Image src="/images/btn-music-search.png" alt="더보기" width={39} height={39} />
+							</button>
+							<input type="text" className="inp-search" />
+						</div>
+					</div>
+
+					<div className="list">
+						<div className="header">
+							<div className="num">
+								<Image src="/images/ic-music-sharp.png" alt="#" width={26} height={30} />
+							</div>
+							<div className="title">
+								Title
+							</div>
+							<div className="album">
+								Album
+							</div>
+							<div className="time">
+								<Image src="/images/ic-music-time.png" alt="#" width={48} height={45} />
+							</div>
+						</div>
+
+						<div className="body">
+							<ul>
+								<li>
+									<div className="num">
+										1
+										<div className="play">
+											<button type="button" className="btn-play">
+												<Image src="/images/ic-music-play.png" alt="플레이" width={42} height={50} />
+											</button>
+										</div>
+									</div>
+									<div className="title">
+										<div className="music">
+											<div className="thumb">
+												<Image src="/images/img-music-cover.png" alt="커버이미지" width={128} height={128} />
+											</div>
+											<div className="subject">
+												<strong>Big Wick Energy</strong>
+												<p>Tyler Bates, Joel J. Richard</p>
+											</div>
+										</div>
+									</div>
+									<div className="album">
+										John Wick Chapter 4
+									</div>
+									<div className="time">
+										3:15
+									</div>
+								</li>
+
+								<li>
+									<div className="num">
+										2
+										<div className="play">
+											<button type="button" className="btn-play">
+												<Image src="/images/ic-music-play.png" alt="플레이" width={42} height={50} />
+											</button>
+										</div>
+									</div>
+									<div className="title">
+										<div className="music">
+											<div className="thumb">
+												<Image src="/images/img-music-cover.png" alt="커버이미지" width={128} height={128} />
+											</div>
+											<div className="subject">
+												<strong>Nowhere To Run</strong>
+												<p>Tyler Bates, Joel J. Richard</p>
+											</div>
+										</div>
+									</div>
+									<div className="album">
+										John Wick Chapter 4
+									</div>
+									<div className="time">
+										2:11
+									</div>
+								</li>
+
+								<li>
+									<div className="num">
+										3
+										<div className="play">
+											<button type="button" className="btn-play">
+												<Image src="/images/ic-music-play.png" alt="플레이" width={42} height={50} />
+											</button>
+										</div>
+									</div>
+									<div className="title">
+										<div className="music">
+											<div className="thumb">
+												<Image src="/images/img-music-cover.png" alt="커버이미지" width={128} height={128} />
+											</div>
+											<div className="subject">
+												<strong>Sand Wick</strong>
+												<p>Tyler Bates, Joel J. Richard</p>
+											</div>
+										</div>
+									</div>
+									<div className="album">
+										John Wick Chapter 4
+									</div>
+									<div className="time">
+										5:12
+									</div>
+								</li>
+
+								<li>
+									<div className="num">
+										4
+										<div className="play">
+											<button type="button" className="btn-play">
+												<Image src="/images/ic-music-play.png" alt="플레이" width={42} height={50} />
+											</button>
+										</div>
+									</div>
+									<div className="title">
+										<div className="music">
+											<div className="thumb">
+												<Image src="/images/img-music-cover.png" alt="커버이미지" width={128} height={128} />
+											</div>
+											<div className="subject">
+												<strong>Change Your Nature</strong>
+												<p>Tyler Bates, Joel J. Richard</p>
+											</div>
+										</div>
+									</div>
+									<div className="album">
+										John Wick Chapter 4
+									</div>
+									<div className="time">
+										4:23
+									</div>
+								</li>
+
+								<li>
+									<div className="num">
+										5
+										<div className="play">
+											<button type="button" className="btn-play">
+												<Image src="/images/ic-music-play.png" alt="플레이" width={42} height={50} />
+											</button>
+										</div>
+									</div>
+									<div className="title">
+										<div className="music">
+											<div className="thumb">
+												<Image src="/images/img-music-cover.png" alt="커버이미지" width={128} height={128} />
+											</div>
+											<div className="subject">
+												<strong>Continental Breakfast</strong>
+												<p>Tyler Bates, Joel J. Richard</p>
+											</div>
+										</div>
+									</div>
+									<div className="album">
+										John Wick Chapter 4
+									</div>
+									<div className="time">
+										2:15
+									</div>
 								</li>
 							</ul>
 						</div>
