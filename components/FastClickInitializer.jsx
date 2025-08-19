bae4d17d8670dc1cb41829371b37142010279e02
channi84 <channi.kr@gmail.com>
2025-08-19 11:41:26 +0900

    ㄴ

--- components/FastClickInitializer.jsx
+++ components/FastClickInitializer.jsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 
 export default function FastClickInitializer() {
 	useEffect(() => {
-		// 브라우저 환경에서만 동적으로 'fastclick'을 불러와서 실행합니다.
 		if (typeof window !== 'undefined') {
 			import('fastclick').then((FastClick) => {
 				FastClick.attach(document.body);
