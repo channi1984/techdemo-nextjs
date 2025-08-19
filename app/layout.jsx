d7c5b2ff1ab6bd5003fd664df4f8d18f9e7a5de6
channi84 <channi.kr@gmail.com>
2025-08-19 13:38:14 +0900

    1

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,6 @@ import "./style.css"
 
 import Header from "@/components/Header";
 import ViewportChanger from "@/components/ViewportChanger";
-import FastClickInitializer from "@/components/FastClickInitializer";
 
 export const viewport = {
 	initialScale: 1.0,
@@ -18,8 +17,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="ko">
 			<body>
-				{/* 패스트 클릭 */}
-				<FastClickInitializer />
 				{/* 뷰 포트 */}
 				<ViewportChanger />
 				{/* 헤더 */}
