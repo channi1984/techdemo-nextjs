fa78aa815487ff63eac5d21be1333cb8247659f4
channi84 <channi.kr@gmail.com>
2025-08-20 10:33:38 +0900

    1

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-	// 사용자가 루트 URL ("/")로 접근하면 "/movie" 페이지로 리다이렉션
-	redirect('/movie');
+	// 사용자가 루트 URL ("/")로 접근하면 "/movie_api" 페이지로 리다이렉션
+	redirect('/movie_api');
 }
