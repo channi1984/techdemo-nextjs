849d531f0438d7409ebf71df267de81eb4a85f64
channi84 <channi.kr@gmail.com>
2026-03-12 17:42:30 +0900

    리다이렉팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/movie',
+				permanent: true, // 영구적인 이동이면 true
+			},
+		];
+	},
+};
 
 export default nextConfig;
