02ad7bfa7c7d5e97f034e6823daff3f90f546dba
channi84 <channi.kr@gmail.com>
2026-03-26 16:42:28 +0900

    포트폴리오 데이터 엄로드

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -9,7 +9,7 @@ const portfolios = [
 		contribute: "기여 100%",
 		period: "2025.05.01 ~ 2025.12-31",
 		description: "인천국제공항의 대고객 서비스 가이드라인 준수 및 웹 접근성 강화 프로젝트",
-		features: ["웹 접근성(WA) 인증 마크 획득, ", "웹표준·웹호환성 준수, ", "공통 UI 컴포넌트 설계, ", "SWIPER 슬라이드"],
+		features: ["웹 접근성(WA) 인증 마크 획득, ", "웹표준·웹호환성 준수, ", "JAVASCRIPT, ", "CSS, ", "SWIPER 슬라이드"],
 		url: "https://airport.kr/sites/ap_ko/index.do",
 		gradation: "radial-gradient(circle, #00b0cc 25%, #0092b7 60%)"
 	}
