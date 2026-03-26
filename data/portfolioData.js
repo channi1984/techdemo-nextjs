d974e963e8be87dc88f4ef22f799fe582f0c0005
channi84 <channi.kr@gmail.com>
2026-03-26 16:57:10 +0900

    포트폴리오 업데이트

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -11,7 +11,21 @@ const portfolios = [
 		description: "인천국제공항의 대고객 서비스 가이드라인 준수 및 웹 접근성 강화 프로젝트",
 		features: ["웹 접근성(WA) 인증 마크 획득, ", "웹표준·웹호환성 준수, ", "JAVASCRIPT, ", "CSS, ", "SWIPER 슬라이드"],
 		url: "https://airport.kr/sites/ap_ko/index.do",
-		gradation: "radial-gradient(circle, #00b0cc 25%, #0092b7 60%)"
+		gradation: "radial-gradient(circle, #00b0cc 0%, #0092b7 90%)"
+	},
+	{
+		id: "2",
+		title: "수협중앙회",
+		imageUrl: "/images/img-port2.png",
+		imageDetailUrl: "/images/img-port-detail2.png",
+		genre: "기관",
+		scope: "UI 퍼블리싱",
+		contribute: "기여 100%",
+		period: "2021.11.05 ~ 2022.04.04",
+		description: "수협중앙회 홈페이지 통합관리 인프라 구축",
+		features: ["웹 접근성(WA) 인증 마크 획득, ", "웹표준·웹호환성 준수, ", "JAVASCRIPT, ", "CSS, ", "SWIPER 슬라이드"],
+		url: "https://www.suhyup.co.kr/suhyup/index.do",
+		gradation: "radial-gradient(circle, #002c77 0%, #001f54 90%)"
 	}
 ];
 
